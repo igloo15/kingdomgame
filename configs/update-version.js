185217a10368499147ebd19d5abb110b481bf88c
igloo15 <mtolland15@gmail.com>
2019-08-31 23:50:28 -0400

    #Add version update script added

---
+++ configs/update-version.js
@@ -0,0 +1,17 @@
+const { gitDescribeSync } = require('git-describe');
+const writePkg = require('write-pkg');
+const readPkg = require('read-pkg');
+
+const gitInfo = gitDescribeSync(__dirname+"/..");
+const pkg = readPkg.sync();
+console.log(gitInfo);
+let version = gitInfo.semver.version;
+
+if(gitInfo.distance > 0) {
+    version = gitInfo.semver.major+"."+(gitInfo.semver.minor+1)+".0"+"-dev."+gitInfo.distance;
+}
+console.log(version);
+
+pkg.version = version;
+
+writePkg.sync(__dirname+"/..", pkg);
